fcfd3496bd63c9cce77607901c711e7cbc4f9216
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-05-08 23:44:36 -0700

    Circle almost done

--- Maria's Xcode Project/Maria's Xcode Project/Collider2D.h
+++ Maria's Xcode Project/Maria's Xcode Project/Collider2D.h
@@ -9,3 +9,44 @@ public:
     virtual Vector3f getA() {return A;};
     virtual Vector3f getB() {return B;};
 };
+
+// Class to represent points.
+class Point {
+private:
+    double xval, yval;
+public:
+    // Constructor uses default arguments to allow calling with zero, one,
+    // or two values.
+    Point(double x = 0.0, double y = 0.0) {
+        xval = x;
+        yval = y;
+    }
+    
+    // Extractors.
+    double x() { return xval; }
+    double y() { return yval; }
+    
+    // Distance to another point.  Pythagorean thm.
+    double dist(Point other) {
+        double xd = xval - other.xval;
+        double yd = yval - other.yval;
+        return sqrt(xd*xd + yd*yd);
+    }
+    
+    // Add or subtract two points.
+    Point add(Point b)
+    {
+        return Point(xval + b.xval, yval + b.yval);
+    }
+    Point sub(Point b)
+    {
+        return Point(xval - b.xval, yval - b.yval);
+    }
+    
+    // Move the existing point.
+    void move(double a, double b)
+    {
+        xval += a;
+        yval += b;
+    }
+};
