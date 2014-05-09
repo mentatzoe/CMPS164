b9fa5abe9d126b29813752b3d34109e3cee2e479
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-05-09 00:20:12 -0700

    Fixed Line Collider

--- Maria's Xcode Project/Maria's Xcode Project/LineCollider.cpp
+++ Maria's Xcode Project/Maria's Xcode Project/LineCollider.cpp
@@ -1,19 +1,11 @@
 #include "LineCollider.h"
 
-float collisionX, collisionZ;
-
-bool intersection(Vector3f p1, Vector3f p2, Vector3f p3, Vector3f p4);
-
-bool LineCollider::getCollision(Collider2D c){
-    return intersection(getA(), getB(), c.getA(), c.getB());
-};
-
-
-bool intersection(Vector3f p1, Vector3f p2, Vector3f p3, Vector3f p4) {
+bool LineCollider::getCollision(Collider2D c, Vector3f& result){
+    //return intersection(getA(), getB(), c.getA(), c.getB());
     // Store the values for fast access and easy
     // equations-to-code conversion
-    float x1 = p1.x, x2 = p2.x, x3 = p3.x, x4 = p4.x;
-    float y1 = p1.z, y2 = p2.z, y3 = p3.z, y4 = p4.z;
+    float x1 = getA().x, x2 = getB().x, x3 = c.getA().x, x4 = c.getB().x;
+    float y1 = getA().z, y2 = getB().z, y3 = c.getA().z, y4 = c.getB().z;
     
     float d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
     // If d is zero, there is no intersection
@@ -30,12 +22,8 @@ bool intersection(Vector3f p1, Vector3f p2, Vector3f p3, Vector3f p4) {
     if ( y < fmin(y1, y2) || y > fmax(y1, y2) ||
         y < fmin(y3, y4) || y > fmax(y3, y4) ) return false;
     
-    collisionX = x;
-    collisionZ = y;
+    result.x = x;
+    result.z = y;
     
     return true;
-};
-
-Vector3f LineCollider::getCollisionPoint(Collider2D c){
-    return Vector3f(collisionX, 0, collisionZ);
 };
--- Maria's Xcode Project/Maria's Xcode Project/LineCollider.h
+++ Maria's Xcode Project/Maria's Xcode Project/LineCollider.h
@@ -7,14 +7,11 @@ public:
     LineCollider(Vector3f a, Vector3f b)
     : A(a)
     , B(b){}
-    bool getCollision(Collider2D c); // Checks if there's been a collision
-    Vector3f getCollisionPoint(Collider2D c); //Returns collision point
+    bool getCollision(Collider2D c, Vector3f& result); // Checks if there's been a collision
     Vector3f getA() {return A;};
     Vector3f getB() {return B;};
 private:
     Vector3f A,B;
-    double Dot(const Vector3f& a,const Vector3f& b)                        { return (a.x*b.x) + (a.y*b.y); }
-    double PerpDot(const Vector3f& a,const Vector3f& b)                    { return (a.y*b.x) - (a.x*b.y); }
 };
 
 
